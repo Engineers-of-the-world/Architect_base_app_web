605337e6ecbf73bb065b64f423813fc9a81bc3ab
jhopes <jhopes.p@gmail.com>
2018-08-20 16:34:12 -0500

    CRUD

--- src/main/java/com/jhopes/configbd/Conexionbd.java
+++ src/main/java/com/jhopes/configbd/Conexionbd.java
@@ -7,12 +7,20 @@ package com.jhopes.configbd;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author P&D
  */
 public class Conexionbd {
+    Connection con; // objeto conexión.    
+    Statement st;
+    ResultSet rs;
     public void Conexionbd() {
         try {
             // We register the PostgreSQL driver
@@ -22,22 +30,38 @@ public class Conexionbd {
             } catch (ClassNotFoundException ex) {
                 System.out.println("Error al registrar el driver de PostgreSQL: " + ex);
             }
-            Connection connection = null;
             // Database connect
             // Conectamos con la base de datos
-            connection = DriverManager.getConnection(
+            con = DriverManager.getConnection(
                     "jdbc:postgresql://localhost:5432/dbprueba",
                     "postgres", "123456");
  
-            boolean valid = connection.isValid(50000);
+            boolean valid = con.isValid(50000);
             System.out.println(valid ? "TEST OK" : "TEST FAIL");
         } catch (java.sql.SQLException sqle) {
             System.out.println("Error: " + sqle);
         }
     } 
-    public static void main(String[] args) {
-        Conexionbd javaPostgreSQLBasic = new Conexionbd();
-        javaPostgreSQLBasic.Conexionbd(); 
+    public ResultSet executeQuery(String sql){
+        try {
+            rs = st.executeQuery(sql);
+            
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexionbd.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rs;
+    }
+    public boolean executeInsertUpdate(String sql){
+        boolean e;
+        try {            
+            st = con.createStatement();
+            st.executeUpdate(sql);
+            e = true;
+        } catch (SQLException ex) {
+            Logger.getLogger(Conexionbd.class.getName()).log(Level.SEVERE, null, ex);
+            e = false;
+        }
+        return e;
     }
     
 }
