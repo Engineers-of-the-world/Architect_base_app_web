01f291d9cc3a442792cc4f2daa55594525e44217
jhopes <jhopes.p@gmail.com>
2018-08-31 11:11:20 -0500

    CRUD + bootfaces+ primefaces + JDBC POSTGRESQL

--- src/main/java/com/jhopes/controller/CategoriaCtrl.java
+++ src/main/java/com/jhopes/controller/CategoriaCtrl.java
@@ -6,48 +6,66 @@
 package com.jhopes.controller;
 
 import com.jhopes.controllerimpl.CategoriaCtrlImpl;
-import com.jhopes.modell.Categoria;
 import java.io.Serializable;
+import com.jhopes.dao.CategoriaDAO;
+import com.jhopes.modell.Categoria;
+import java.util.ArrayList;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
-import com.jhopes.dao.CategoriaDAO;
 
 /**
  *
  * @author P&D
  */
-@ManagedBean
+@ManagedBean(name = "categoriaCtrl")
 @SessionScoped
-public class CategoriaCtrl implements Serializable{
+public class CategoriaCtrl implements Serializable {
 
     private Categoria selected;
     private boolean est;
     CategoriaDAO catdao;
+    private ArrayList<Categoria> list = new ArrayList<>();
 
-    public void createCategoria() {
-        catdao = new CategoriaCtrlImpl();
-        System.out.println("name categoria: " + selected.name_cat);
-        //est = catdao.createCategoria(selected);
+    public CategoriaCtrl() {
+        this.selected = new Categoria();
+        this.catdao = new CategoriaCtrlImpl();
     }
 
-    /**
-     *
-     * @return
-     */
-    public Categoria prepareCreate() {
-        //selected = new Categoria();
-        initializeEmbeddableKey();
+    public void createCategoria() {        
+        if (catdao.createCategoria(selected)) {
+            System.out.println("Insertado");
+        } else {
+            System.out.println("Error al momento de insertar");
+        }
+    }
+
+    public ArrayList<Categoria> getList() {
+        list = catdao.readCategoria();
+        return list;
+    }
+
+    public void updateCategoria(Categoria cat) {
+        System.out.println("categoria = "+cat.name_cat);
+        if (catdao.updateCategoria(cat)) {
+            System.out.println("Actualizado");
+        } else {
+            System.out.println("Error al momento de actualizar");
+        }
+    }
+    public void deleteCategoria(String id){
+        
+        if (catdao.deleteCategoria(id)) {
+            System.out.println("Eliminado"+id);
+        } else {
+            System.out.println("Error al momento de eliminar");
+        }
+    }
+    public Categoria getSelected() {
         return selected;
     }
-    public Categoria getCategoria() {
-        return selected;
-    }
 
-    public void setCategoria(Categoria categoria) {
-        this.selected = categoria;
-    }
-
-    private void initializeEmbeddableKey() {        
+    public void setSelected(Categoria selected) {
+        this.selected = selected;
     }
 
 }
